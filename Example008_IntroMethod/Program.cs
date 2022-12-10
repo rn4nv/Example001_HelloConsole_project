c5e8a8b198983eb9ae9ceb67bcb14e2b96b39bfe
rn4nv <dorjuan@ya.ru>
2022-12-11 02:51:04 +0700

    Example 008 Intro Method

---
+++ Example008_IntroMethod/Program.cs
@@ -0,0 +1,54 @@
+﻿Console.Clear();
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+
+int a1 = 34;
+int b1 = 12;
+int c1 = 24;
+int a2 = 65;
+int b2 = 41;
+int c2 = 81;
+int a3 = 36;
+int b3 = 96;
+int c3 = 27;
+
+// Начальный вариант.
+
+// int max = a1;
+// if (b1 > max) max = b1;
+// if (c1 > max) max = c1;
+
+// if (a2 > max) max = a2;
+// if (b2 > max) max = b2;
+// if (c2 > max) max = c2;
+
+// if (a3 > max) max = a3;
+// if (b3 > max) max = b3;
+// if (c3 > max) max = c3;
+
+// Console.WriteLine(max);
+
+// Первое преобразование.
+
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// int max = Max(max1, max2, max3);
+
+// Console.WriteLine(max);
+
+// Второе преобразование.
+
+int max = Max(
+    Max(a1, b1, c1),
+    Max(a2, b2, c2),
+    Max(a3, b3, c3));
+return max;
+
+Console.WriteLine(max);
